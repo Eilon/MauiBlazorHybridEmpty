fcb8c955ef3493a0ff01ee88c34dc5171481130c
Eilon Lipton <Eilon@users.noreply.github.com>
2024-07-15 10:58:39 -0700

    Initial commit

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiBlazorHybridEmpty;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
